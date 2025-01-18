458a5ff6059c7b9ece7175d73ee106f8eb1a5f8e
gd388 <dixitg7388@gmail.com>
2025-01-19 04:51:43 +0530

    ..

--- core/settings.py
+++ core/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_ACCEPT_CONTENT = ['json']  
+CELERY_TASK_SERIALIZER = 'json'  
+CELERY_RESULT_SERIALIZER = 'json' 
+CELERY_TIMEZONE = 'UTC'   
+CELERY_RESULT_EXPIRES = 3600*24
